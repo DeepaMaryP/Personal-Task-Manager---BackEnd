71e77fbd81591d40ca5ab75d27fa9d7085e965ee
DESKTOP-KA4OTAB\mr <deepamary.p@gmail.com>
2025-09-07 18:45:57 +0530

    added new route for testing

--- app.js
+++ app.js
@@ -13,6 +13,10 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
+
 app.use('/api/auth', userRouter)
 app.use('/api/tasks', taskRouter)
 
